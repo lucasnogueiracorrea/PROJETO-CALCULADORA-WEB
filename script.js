72f1d7dfa84883b0daef332242f32d7bb39c5555
Lucas Corrêa <80265794+lucasnogueiracorrea@users.noreply.github.com>
2022-05-11 17:05:11 -0300

    atualizei o arq js

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
-function calculadora(){
-    var n1 = Number(window.document.getElementById('txt1'))
-    var n2 = Number(window.document.getElementById('txt2'))
-    var txt3 = Number(window.document.getElementById('txt3'))
-    var res
-    var btn = window.document.getElementById('btn')
-    
+var n1 = window.document.getElementById('txt1')
+var n2 = window.document.getElementById('txt2')
+var resposta = window.document.querySelector('span')
 
-    function somar(){
-        res = n1 + n2
-    }
-    function subtrair(){
-        res = n1 - n2
-    }
-    function dividir(){
-        res = n1 / n2
-    }
-    function multiplicar(){
-        res = n1 * n2
-    }
-
-    function check(){
-        if(txt3 == 1){
-            somar()
-        } else if(txt3 == 2){
-            subtrair()
-        } else if(txt3 == 3){
-            dividir()
-        } else if(txt3 == 4){
-            multiplicar()
-        } else if(txt3 >4){
-            alert("Você digitou uma opção inválida.")
-        }
-    }
-
-    btn.addEventListener('click' function{
-        check()
-        
-
-    })
-    
-}    
-
-calculadora();
+function soma(){
+    resposta.innerHTML = parseInt(n1.value) + parseInt(n2.value)
+}
+function subtracao(){
+    resposta.innerHTML = parseInt(n1.value) - parseInt(n2.value)
+}
+function divisao(){
+    resposta.innerHTML = parseInt(n1.value) / parseInt(n2.value)
+}
+function multiplicacao(){
+    resposta.innerHTML = parseInt(n1.value) * parseInt(n2.value)
+}
